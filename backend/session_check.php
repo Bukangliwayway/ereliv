feeed8c60239aab74ef080fd4608f34280403d52
bukangliwayway <jamesmatthewbelgica@gmail.com>
2023-05-29 15:34:35 +0800

    Modified Session Var

--- backend/session_check.php
+++ backend/session_check.php
@@ -5,9 +5,9 @@ include '../db/queries.php';
 // Start the session
 session_start();
 // Check if the session is null or empty
-if (empty($_SESSION['user_id'])) {
-  // Redirect the user back to the login page or show an error message
-  send_message_and_redirect("Please Login First!", "/ereliv/rbac.php");
+if (empty($_SESSION['userID'])) {
+  // Redirect the user back to the login page 
+  send_message_and_redirect("Please Login First!", "/ereliv/");
 
 
   exit(); // Stop further execution of the script
